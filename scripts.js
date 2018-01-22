1dd59c9ffebe8d451fcfcd99973e47e0a1ed27e9
Jennah <contactjennah@gmail.com>
2018-01-22 14:26:37 +0000

    Fix bug where users could not enter mulitple zeros after a decimal point

--- scripts.js
+++ scripts.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded",function(){
 
           //preventing multiple zeros unless after decimal point or integers
           if(this.id == "zero") {
-            if(answer[0] == 0 || [answer[answer.length-2], answer[answer.length-1]].join("").match(/[√\-x+/](?=0{1})/g)) {
+            if(answer[0] == 0 && answer.length == 1 || !/[0-9\.]/.test(answer[answer.length-2])) {
               addClass(this, "invalid");
             }
           } else {
